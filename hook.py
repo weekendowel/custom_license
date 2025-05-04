ac8eb8ac61dfd636e6fe6fe1458316d70b244fd3
weekendowel <johan.zvensson@gmail.com>
2025-05-04 13:24:00 +0200

    Delete hook.py

--- hook.py
+++
@@ -1,7 +0,0 @@
-# custom_license/hooks.py
-app_name = "custom_license"
-app_title = "Custom License"
-app_description = "Custom License App"
-app_publisher = "ArrivalNet"
-app_email = "support@arrivalnet.se"
-app_license = "MIT"
