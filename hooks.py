2048d2dab1bbd5f5cdda577742136604654c7ed1
weekendowel <johan.zvensson@gmail.com>
2025-05-04 13:17:46 +0200

    Create hooks.py

---
+++ hooks.py
@@ -0,0 +1,7 @@
+# custom_license/hooks.py
+app_name = "custom_license"
+app_title = "Custom License"
+app_description = "Custom License App"
+app_publisher = "ArrivalNet"
+app_email = "support@arrivalnet.se"
+app_license = "MIT"
